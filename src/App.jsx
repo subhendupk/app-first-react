768f274a4b8fe7fff03d33e46c2562aa0c5d6564
subhendupk <subhendu.sett@weavers-web.com>
2023-12-08 12:55:46 +0530

    Vite configure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import StudentList from "./pages/StudentList";
+
+function App() {
+
+  return (
+    <StudentList />
+  )
+}
+
+export default App
--- src/components/DisplayBox.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function DisplayBox({text}) {
-  return (
-    <div id="print-number">{text}</div>
-  )
-}
